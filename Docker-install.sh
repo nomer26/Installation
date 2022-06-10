433d4e576ae796538fecf67dbdaca5e32ce6d385
nomer26 <rsh4297@gmail.com>
2022-06-10 12:53:38 +0000

    Docker guide

--- Docker-install.sh
+++ Docker-install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+function pc(){
+  nc='\033[0m' # no color
+
+  case $1 in
+        "green") COLOR='\033[0;32m';;
+        "red") COLOR='\033[0;31m';;
+        "*") COLOR='\033[0m';;
+  esac
+  echo -e "${COLOR} $2 ${nc}"
+}
+
+
 
 ## SW pkg for APT Repository 
+pc "green" "
+###############################\n\
+## SW pkg for APT Repository ##\n\
+###############################"
 
 sudo apt-get update -y
 
@@ -9,6 +25,10 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-relea
 
 
 ## Add Docker Repository  
+pc "green" "
+###############################\n\
+## Add Docker Repository     ##\n\
+###############################"
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
@@ -21,9 +41,13 @@ sudo apt-get update -y
 
 
 ## Install Docker Enging &  Container Runtime 
+pc "green" "
+###################################################\n\
+## Install Docker Enging & Container Runtime     ##\n\
+###################################################"
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-
+pc "green" " Finished  Docker Installation... "
 
 
